0045046edc1aa6b540d3a7577d82f80c203242fa
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-04-08 13:44:45 +0200

    styling search results pet component

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,15 @@
 import { createRoot } from "react-dom/client";
-// import Pet from './Pet'
 import SearchParams from './SearchParams'
 
 const App = () => {
   return (
     <div>
       <h1>Adopt me!</h1>
-      <SearchParams/>
+      <SearchParams />
     </div>
   )
 };
 
-// Embed app in HTML
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(< App />);
--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,17 +1,20 @@
-const Pet = (props) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "http://pets-images.dev-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
+
   return (
-    <div>
-      <h1> {props.name} </h1>
-      <h2> {props.animal} </h2>
-      <h2> {props.breed} </h2>
-    </div>
-  )
-}
+    <a href={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name}/>
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>{animal} - {breed} - {location}</h2>
+      </div>
+    </a>
+  );
+};
 
 export default Pet;
-
-// Use of props to use properties of instances
-// In app you would specify in <div> <AppName prop="" />:
-// - <Pet name="Tommy" animal="Cat" breed="Tuxedo cat" />
-// - <Pet name="Bobby" animal="Dog" breed="Dachshund" />
-// - <Pet name="Sara" animal="Guinea pig" breed="Shorthair" />
